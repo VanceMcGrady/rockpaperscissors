44598e4e54910c4e83e6e200c74962e6218a9740
Vance McGrady <vancemcgrady@gmail.com>
2021-11-29 19:09:44 -0700

    application working without bugs

--- app.js
+++ app.js
@@ -1,24 +1,63 @@
-const playerAnswer = document.querySelector(".player-answer").innerHTML;
-const computerAnswer = document.querySelector(".computer-answer").innerHTML;
+let playerAnswer = document.querySelector(".player-answer");
+let computerAnswer = document.querySelector(".computer-answer");
+let outcome = document.querySelector(".outcome");
 const rockBtn = document.querySelector(".rock");
 const paperBtn = document.querySelector(".paper");
 const scissorsBtn = document.querySelector(".scissors");
 
+const playBtn = document.querySelector(".play-btn");
+
 function computerPlay() {
-  let move = "";
   let randNum = Math.floor(Math.random() * 3);
+  let compMove = "";
 
   switch (randNum) {
     case 0:
-      return "rock";
+      compMove = "rock";
       break;
     case 1:
-      return "paper";
+      compMove = "paper";
       break;
     case 2:
-      return "scissors";
+      compMove = "scissors";
       break;
     default:
       null;
   }
+  computerAnswer.innerHTML = compMove;
+  return compMove;
 }
+
+function playerPlay() {}
+rockBtn.addEventListener("click", () => (playerAnswer.innerHTML = "rock"));
+paperBtn.addEventListener("click", () => (playerAnswer.innerHTML = "paper"));
+scissorsBtn.addEventListener(
+  "click",
+  () => (playerAnswer.innerHTML = "scissors")
+);
+
+function gameLogic() {
+  compMove = computerPlay();
+  playerMove = playerAnswer.innerHTML;
+
+  if (!playerMove) {
+    outcome.innerHTML = "You Must Enter A Move To Play";
+    computerAnswer.innerHTML = "";
+  } else if (playerMove == "scissors" && compMove == "rock") {
+    outcome.innerHTML = "You Lose!";
+  } else if (playerMove == "scissors" && compMove == "paper") {
+    outcome.innerHTML = "You Win!";
+  } else if (playerMove == "paper" && compMove == "scissors") {
+    outcome.innerHTML = "You Lose!";
+  } else if (playerMove == "paper" && compMove == "rock") {
+    outcome.innerHTML = "You Win!";
+  } else if (playerMove == "rock" && compMove == "paper") {
+    outcome.innerHTML = "You Lose!";
+  } else if (playerMove == "rock" && compMove == "scissors") {
+    outcome.innerHTML = "You Win!";
+  } else {
+    outcome.innerHTML = "Tie";
+  }
+}
+
+playBtn.addEventListener("click", gameLogic);
